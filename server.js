ee1ef953a1924b49f1534ab4dc3e851d7ae66f32
mannycolon <colonmanuel7@gmail.com>
2017-04-08 20:09:24 -0400

    initial commit, created a hello world server

---
+++ server.js
@@ -0,0 +1,14 @@
+// http://expressjs.com/en/starter/hello-world.html
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello World! \n testing server...')
+})
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!")
+})
+
+// to test run node server.js 
+// Then, load http://localhost:3000/ in a browser to see the output.
